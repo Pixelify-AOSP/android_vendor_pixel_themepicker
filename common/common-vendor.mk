28a194eb2eeddb8985723501a1688a9b71d3c356
Ashwin Devi Srinivasan <astroashwin@outlook.com>
2026-07-03 18:16:26 +0000

    themepicker: regen from blazer-ota-cp2a.260605.012

--- common/common-vendor.mk
+++ common/common-vendor.mk
@@ -6,8 +6,8 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/pixel/themepicker/common
 
 PRODUCT_PACKAGES += \
-    PixelThemesStub2022_and_newer \
-    PixelWallpapers2024 \
+    PixelThemesStub2025 \
+    PixelWallpapers2025 \
     WallpaperAIPrebuilt_10000372 \
     WallpaperEmojiPrebuilt-foldable-wallpaper \
     PixelLiveWallpaperPrebuilt-26000013 \
